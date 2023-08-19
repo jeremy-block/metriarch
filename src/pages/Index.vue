c0f7a453728ca7cff7b897502219e151e3d12efc
Jeremy Block <j.block@ufl.edu>
2023-08-19 14:26:48 -0400

    duplicating main plot to remove features. clean start.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -59,6 +59,7 @@
         <SelectionPanel/>
         <FilterBar />
         <div class="flex">
+            <wonderplot ref="wonderful"/>
             <Scatterplot title="Name of Chart" :xAccessor="DoXThing()" :yAccessor="DoYThing()" ref="plot" />
             <Session />
         </div>
